fc845408f8f36aeb214e1e1f0a495971c90022be
hi <huanxuan_oh@mymail.sutd.edu.sg>
2020-10-06 13:50:37 +0800

    test

--- scripts/pagescript.js
+++ scripts/pagescript.js
@@ -3,19 +3,6 @@ window.onload = function () {
   //   document.body.style.color =
   //     localStorage.getItem("storetoggle") == "true" ? "#ccc" : "#303030";
   toggleDark();
-
-  // iframe dark mode //
-  //   window.frames[0].document.body.style.color = toggle ? "#303030" : "#ccc";
-  //   proj_list = window.frames[0].document.getElementsByClassName("proj");
-  //   for (let i = 0; i < proj_list.length; i++) {
-  //     proj_list[i].style.background = toggle ? "#eee" : "#222";
-  //   }
-
-  //   if (window.frames[0].document.getElementsByTagName("h1")[0]) {
-  //     window.frames[0].document.getElementsByTagName("h1")[0].style.color = toggle
-  //       ? "rgb(211, 181, 156)"
-  //       : "rgb(217, 193, 132)";
-  //   }
 };
 
 function toggleDark() {
